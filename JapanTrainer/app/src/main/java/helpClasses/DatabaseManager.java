a3b3fa285f916b786454151bfe7fb4a3247cb4f3
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-10 20:28:23 +0200

    Implemented Syllables into DatabaseManager

--- JapanTrainer/app/src/main/java/helpClasses/DatabaseManager.java
+++ JapanTrainer/app/src/main/java/helpClasses/DatabaseManager.java
@@ -12,6 +12,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
 
+import helpClasses.syllableClasses.HiraganaSyllable;
+import helpClasses.syllableClasses.KatakanaSyllable;
+import helpClasses.syllableClasses.Syllable;
 import helpClasses.wordClasses.HiraganaWord;
 import helpClasses.wordClasses.KatakanaWord;
 import helpClasses.wordClasses.Word;
@@ -21,7 +24,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     SQLiteDatabase db;
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "Words.db";
+    private static final String DATABASE_NAME = "Japanese.db";
 
     // Both
     private static final String KEY_ID = "ID";
@@ -40,6 +43,16 @@ public class DatabaseManager extends SQLiteOpenHelper {
     private static final String HIRAGANA = "HIRAGANA";
     private static final String KANJI = "KANJI";
 
+    // Syllable Section
+
+    // Katakana
+    // columns - ID (Primary key) | Katakana | Romaji
+    private static final String TABLE_NAME_KATAKANA_SYLLABLES = "katakana_syllable";
+
+    // Hiragana
+    // columns - ID (Primary key) | Hiragana | Romaji
+    private static final String TABLE_NAME_HIRAGANA_SYLLABLES = "hiragana_syllable";
+
     // Constructor
     public DatabaseManager (Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -49,8 +62,19 @@ public class DatabaseManager extends SQLiteOpenHelper {
     // Function that was in parent class
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        // Create Katakana Syllables
+        String createTable = "create table " + TABLE_NAME_KATAKANA_SYLLABLES +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, KATAKANA TEXT, ROMAJI TEXT)";
+        db.execSQL(createTable);
+
+        // Create Hiragana Syllables
+        createTable = "create table " + TABLE_NAME_HIRAGANA_SYLLABLES +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, HIRAGANA TEXT, ROMAJI TEXT)";
+        db.execSQL(createTable);
+
         // Create Katakana
-        String createTable = "create table " + TABLE_NAME_KATAKANA +
+        createTable = "create table " + TABLE_NAME_KATAKANA +
                 " (ID INTEGER PRIMARY KEY AUTOINCREMENT, KATAKANA TEXT, ROMAJI TEXT, " +
                 " TRANSLATION TEXT, IMAGE BLOB)";
         db.execSQL(createTable);
@@ -67,9 +91,51 @@ public class DatabaseManager extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //Drop older table if exist
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_KATAKANA_SYLLABLES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_HIRAGANA_SYLLABLES);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_KATAKANA);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_HIRAGANA);
         onCreate(db);
+    }
+
+    // Inserts a syllable into database
+    public boolean insertSyllable(Syllable syllable){
+
+        if (syllable instanceof KatakanaSyllable){
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+
+            // putting values into columns
+            contentValues.put(KATAKANA, ((KatakanaSyllable) syllable).getKatakana());
+            contentValues.put(ROMAJI, syllable.getRomaji());
+
+            long result = db.insert(TABLE_NAME_KATAKANA, null, contentValues);
+
+            if (result == -1){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+
+        else {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+
+            // putting values into columns
+            contentValues.put(HIRAGANA, ((HiraganaSyllable) syllable).getHiragana());
+            contentValues.put(ROMAJI, syllable.getRomaji());
+
+            long result = db.insert(TABLE_NAME_HIRAGANA, null, contentValues);
+
+            if (result == -1){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
     }
 
     // Inserts a word into database
@@ -119,6 +185,40 @@ public class DatabaseManager extends SQLiteOpenHelper {
         }
     }
 
+    // Takes a syllable from database with the ID
+    public Syllable getSyllable(int id, String font){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        if (font.equals("katakana")){
+            Cursor cursor = db.query(TABLE_NAME_KATAKANA_SYLLABLES, new String[] {KEY_ID,
+                            KATAKANA, ROMAJI}, KEY_ID +
+                            "=?", new String[]{String.valueOf(id)},
+                    null, null, null, null);
+
+            if (cursor != null){
+                cursor.moveToFirst();
+            }
+
+            KatakanaSyllable syllable = new KatakanaSyllable(cursor.getString(1), cursor.getString(2));
+
+            return syllable;
+        }
+        else{
+            Cursor cursor = db.query(TABLE_NAME_HIRAGANA_SYLLABLES, new String[] {KEY_ID,
+                            HIRAGANA, ROMAJI}, KEY_ID +
+                            "=?", new String[]{String.valueOf(id)},
+                    null, null, null, null);
+
+            if (cursor != null){
+                cursor.moveToFirst();
+            }
+
+            HiraganaSyllable syllable = new HiraganaSyllable(cursor.getString(1), cursor.getString(2));
+
+            return syllable;
+        }
+    }
+
     // Takes a word from database with the ID
     public Word getWord(int id, String font){
         SQLiteDatabase db = this.getReadableDatabase();
@@ -153,7 +253,44 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
             return word;
         }
+    }
 
+    // Gives back all the IDs of the syllables in a Database
+    public ArrayList<Integer> getAllIdSyllable(String font){
+        ArrayList<Integer> IdList = new ArrayList<>();
+
+        if (font.equals("katakana")){
+            String selectQuery = "SELECT ID FROM " + TABLE_NAME_KATAKANA_SYLLABLES;
+
+            SQLiteDatabase db = this.getWritableDatabase();
+            Cursor cursor = db.rawQuery(selectQuery, null);
+
+            if(cursor.moveToFirst()){
+                do {
+
+                    int tmp = Integer.parseInt(cursor.getString(0));
+
+                    IdList.add(tmp);
+                } while (cursor.moveToNext());
+            }
+            return IdList;
+        }
+        else {
+            String selectQuery = "SELECT ID FROM " + TABLE_NAME_HIRAGANA_SYLLABLES;
+
+            SQLiteDatabase db = this.getWritableDatabase();
+            Cursor cursor = db.rawQuery(selectQuery, null);
+
+            if(cursor.moveToFirst()){
+                do {
+
+                    int tmp = Integer.parseInt(cursor.getString(0));
+
+                    IdList.add(tmp);
+                } while (cursor.moveToNext());
+            }
+            return IdList;
+        }
 
 
     }
@@ -202,6 +339,8 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public void deleteDB(){
         SQLiteDatabase db = this.getWritableDatabase();
 
+        db.execSQL("DROP TABLE " + TABLE_NAME_KATAKANA_SYLLABLES);
+        db.execSQL("DROP TABLE " + TABLE_NAME_HIRAGANA_SYLLABLES);
         db.execSQL("DROP TABLE " + TABLE_NAME_KATAKANA);
         db.execSQL("DROP TABLE " + TABLE_NAME_HIRAGANA);
         onCreate(db);
@@ -209,21 +348,48 @@ public class DatabaseManager extends SQLiteOpenHelper {
 
     // Check if table Words is empty
     public boolean isEmpty(){
+
+        int icount;
+
         SQLiteDatabase db = getWritableDatabase();
-        String count = "SELECT count(*) FROM " + TABLE_NAME_KATAKANA;
+        String count = "SELECT count(*) FROM " + TABLE_NAME_KATAKANA_SYLLABLES;
 
         Cursor mcursor = db.rawQuery(count, null);
         mcursor.moveToFirst();
-        int icount_katakana = mcursor.getInt(0);
+        icount = mcursor.getInt(0);
+
+        if (icount > 0)
+            return false;
+
+        db = getWritableDatabase();
+        count = "SELECT count(*) FROM " + TABLE_NAME_HIRAGANA_SYLLABLES;
+
+        mcursor = db.rawQuery(count, null);
+        mcursor.moveToFirst();
+        icount = mcursor.getInt(0);
+
+        if (icount > 0)
+            return false;
+
+        db = getWritableDatabase();
+        count = "SELECT count(*) FROM " + TABLE_NAME_KATAKANA;
+
+        mcursor = db.rawQuery(count, null);
+        mcursor.moveToFirst();
+        icount = mcursor.getInt(0);
+
+        if (icount > 0)
+            return false;
 
         count = "SELECT count(*) FROM " + TABLE_NAME_HIRAGANA;
 
         mcursor = db.rawQuery(count, null);
         mcursor.moveToFirst();
-        int icount_hiragana = mcursor.getInt(0);
+        icount = mcursor.getInt(0);
 
-        if (icount_katakana > 0 && icount_hiragana > 0)
+        if (icount > 0)
             return false;
+
         return true;
     }
 
