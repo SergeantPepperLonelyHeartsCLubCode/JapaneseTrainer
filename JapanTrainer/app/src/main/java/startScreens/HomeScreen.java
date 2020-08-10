4ac256805dd79a50fc3f74e5a93edeb588dce9b8
SeargentPepperLonelyHeartsClubCode <ddreier36@gmail.com>
2020-08-10 16:29:38 +0200

    Splashscreen
    Coomit with animation splashscreen

--- JapanTrainer/app/src/main/java/startScreens/HomeScreen.java
+++ JapanTrainer/app/src/main/java/startScreens/HomeScreen.java
@@ -9,8 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import helpClasses.DatabaseManager;
@@ -25,12 +29,35 @@ public class HomeScreen extends AppCompatActivity {
 
     TextView textView;
     PointsManager points;
-
+    Animation homescreen_animation;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_home);
+
+
+        homescreen_animation= AnimationUtils.loadAnimation(this,R.anim.homescreen_animation);
+        image=findViewById(R.id.imageView2);
+        image.setAnimation(homescreen_animation);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent homeIntent = new Intent(HomeScreen.this, HomeScreen.class);
+//https://stackoverflow.com/questions/13397709/android-hide-imageview
+                ImageView imgView = (ImageView)findViewById(R.id.imageView2);
+                imgView .setVisibility(View.GONE);
+
+
+
+            }
+        }, 1000);
+
+
+
 
         // Initilializing
         points = new PointsManager(this);
