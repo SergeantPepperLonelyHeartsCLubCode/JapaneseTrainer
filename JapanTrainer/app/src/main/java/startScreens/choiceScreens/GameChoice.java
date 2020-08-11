20078c73eeced44dc36bfdf8c3df7c854660dc34
Dumbyarnik <daniel.vorobyev@zoho.eu>
2020-08-11 22:40:09 +0200

    Created Beta Version of the screen

--- JapanTrainer/app/src/main/java/startScreens/choiceScreens/GameChoice.java
+++ JapanTrainer/app/src/main/java/startScreens/choiceScreens/GameChoice.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.japantrainer.R;
+import com.google.android.material.card.MaterialCardView;
 
 import gameScreens.QuizGame;
 import gameScreens.TextGame;
@@ -21,9 +22,10 @@ import helpClasses.managerClasses.PointsManager;
 
 public class GameChoice extends AppCompatActivity implements View.OnClickListener {
 
-    PointsManager points;
+    private PointsManager points;
     private Toolbar toolbar;
     private TextView textView;
+    private String game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +35,64 @@ public class GameChoice extends AppCompatActivity implements View.OnClickListene
         // Initialization
         points = new PointsManager(this);
 
+        final Button next = findViewById(R.id.next);
+        final MaterialCardView text_game = findViewById(R.id.text_game);
+        final MaterialCardView quiz_game = findViewById(R.id.quiz_game);
+
         // Setting Toolbar
         toolbar = findViewById(R.id.homescreen_toolbar);
         setSupportActionBar(toolbar);
         //Setting up the back button
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Setting points from to TextView
         textView = findViewById(R.id.points);
-        textView.setText(String.valueOf(points.getPoints()));
+        //textView.setText(String.valueOf(points.getPoints()));
 
-        // Setting the buttons
-        Button text_game = findViewById(R.id.text_game);
-        Button quiz_game = findViewById(R.id.quiz_game);
+        final Intent intent = new Intent(this, TextGame.class);
+        final Intent intent2 = new Intent(this, QuizGame.class);
 
-        text_game.setOnClickListener(this);
-        quiz_game.setOnClickListener(this);
+        text_game.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                if (!text_game.isChecked()){
+                    text_game.toggle();
+                    quiz_game.setChecked(false);
+                    next.setEnabled(true);
+                    game = "text_game";
+                }
+            }
+        });
+
+        quiz_game.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                if (!quiz_game.isChecked()){
+                    quiz_game.toggle();
+                    text_game.setChecked(false);
+                    next.setEnabled(true);
+                    game = "quiz_game";
+
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Setting points to 0
+                points.setPointsNull();
+
+                if (game.equals("text_game")){
+                    startActivity(intent);
+                }
+                else{
+                    startActivity(intent2);
+                }
+
+
+            }
+        });
     }
 
     @Override
